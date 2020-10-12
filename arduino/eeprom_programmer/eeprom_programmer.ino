29ef04060dbb2ea1331e5099cd3ccb8a62541b9a
sardylan <sardylan@gmail.com>
2020-10-12 23:27:51 +0200

    Replace header in source files

--- arduino/eeprom_programmer/eeprom_programmer.ino
+++ arduino/eeprom_programmer/eeprom_programmer.ino
@@ -1,3 +1,22 @@
+/*
+ * qFM1000
+ * Copyright (C) 2020  Luca Cireddu (sardylan@gmail.com)
+ * https://www.lucacireddu.it/
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #include <Arduino.h>
 #include <Wire.h>
 
