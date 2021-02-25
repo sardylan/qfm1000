0e3a63937db5235fc593a065fb70d26ed6469d13
sardylan <sardylan@gmail.com>
2021-02-25 22:31:18 +0100

    Fixes test data

--- src/widgets/sample/main.cpp
+++ src/widgets/sample/main.cpp
@@ -18,8 +18,18 @@ int main(int argc, char **argv) {
     scrollArea.setWidgetResizable(true);
     scrollArea.show();
 
-    QMetaObject::invokeMethod(&hexEditor, "setData", Qt::QueuedConnection, Q_ARG(QByteArray, QByteArray(
-            "\x00\x01\x02\x03\x5ciaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociaociao")));
+    QByteArray data = QByteArray(
+            "\x00\x01\x02\x03\x04\xa6\x3d\xa2"
+            "\x34\xb2\x1e\x48\xeb\x26\x78\x3c"
+            "\x68\x6a\xa6\xb2\xf2\xb9\x8b\xf2"
+            "\x31\x7a\x1b\xd2\x56\x6b\xc1\x4f"
+            "\x2e\x14\x3e\x2b\xeb\x46\xa7\x45"
+            "\xd2\x1c\x3c\x0a\xb1\xbe\x1b\x33"
+            "\xa7\xd5\x70\x98\x94\x7c\xf6\x22",
+            56
+    );
+
+    QMetaObject::invokeMethod(&hexEditor, "setData", Qt::QueuedConnection, Q_ARG(QByteArray, data));
 
     return QApplication::exec();
 }
